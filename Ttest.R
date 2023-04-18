e25e056d1aa61bf2aa2d2b9f5bc7bab849c4ee73
jvance7-ut <73716667+jvance7-ut@users.noreply.github.com>
2023-04-17 23:18:26 -0600

    Update Ttest.R

--- Ttest.R
+++ Ttest.R
@@ -14,13 +14,13 @@
 #' @export
 #'
 #' @examples
-tConstr = function(x, y, alpha, independent = TRUE, equalVar = TRUE){
+tConstr = function(x, y, alpha, independentSamp = TRUE, equalVar = TRUE){
 
-  if(independent=TRUE && equalVar = TRUE){
+  if(independentSamp==TRUE && equalVar == TRUE){
     #run the t-test with equal var
     ttest = t.test(x, y, var.equal = TRUE)
   }
-  else if(independent=TRUE && equalVar=FALSE){
+  else if(independentSamp==TRUE && equalVar==FALSE){
     #run the t-test with unequal var
     ttest = t.test(x, y, var.equal = TRUE)
   }
@@ -29,7 +29,7 @@ tConstr = function(x, y, alpha, independent = TRUE, equalVar = TRUE){
   }
   #create the data frame of x and y
   #x and y won't always be the same length and may return an error
-  #check the lengths of x and y and make corrections to lenght if needed with NA values
+  #check the lengths of x and y and make corrections to length if needed with NA values
   if(length(x)==length(y)){
     df = data.frame(x = x, y = y)
   }
